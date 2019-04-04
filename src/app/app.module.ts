664b4b4fb606103f87e5f99615830a083d1109d9
Lucas Barros <lucas-barros28@hotmail.com>
2019-04-04 20:33:56 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// PrimeNg
+import { MenubarModule } from 'primeng/components/menubar/menubar';
+import { ButtonModule } from 'primeng/components/button/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
+    // PrimeNg
+    MenubarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  items: MenuItem[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Início',
+        items: [
+          { label: 'Interessados' },
+          { label: 'Interessados(Eventos)' },
+          { label: 'Matrículas' },
+          { label: 'Frequências' },
+          { label: 'Alunos' },
+        ]
+      },
+      {
+        label: 'Financeiro',
+        items: [
+          { label: 'Títulos'},
+          { label: 'Estoque'}
+        ]
+      },
+      {
+        label: 'Relatórios',
+        items: [
+          { label: 'Matrículas' },
+          { label: 'Financeiro' },
+          { label: 'Frequências' },
+          { label: 'Interessados' },
+          { label: 'Professores' },
+        ]
+      },
+      {
+        label: 'Administração',
+        items: [
+          { label: 'Grupo de acesso' },
+          { label: 'Papeis' },
+          { label: 'Utilizadores' }
+        ]
+      },
+      {
+        label: 'Ajuda',
+        items: [
+          { label: 'Abrir chamado' },
+          { label: 'Configurações' },
+          { label: 'Sobre' },
+          { label: 'Suporte', url: 'http://google.com' }
+        ]
+      },
+      { label: 'Login',  routerLink: '/login' }
+    ];
+  }
+
+}
